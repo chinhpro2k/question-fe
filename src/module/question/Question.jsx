d71d43059bbfd93660498aad4135d5579a111460
chinhcd <7a3dqh@gmail.com>
2021-11-27 21:59:00 +0700

    fist

---
+++ src/module/question/Question.jsx
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react'
+
+function Question(props){
+    useEffect(()=>{
+
+    },[])
+return(
+    <div>
+        chinh
+    </div>
+)
+}
+
+export default Question();
